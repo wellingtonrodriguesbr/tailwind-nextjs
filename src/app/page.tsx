f1837fc520a2c0376a81f21a0e8a7472d4873f90
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-08-02 19:44:44 -0300

    initial config

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-neutral-800">
+      <h1 className="text-sky-500 text-9xl">Hello World!</h1>
+    </main>
+  );
+}
