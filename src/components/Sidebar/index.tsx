96c453c8aa4852463355499a7da498a582262d00
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-08-18 06:22:45 -0300

    refactor: refactor Input to pattern composition

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -13,19 +13,18 @@ import { Logo } from "./Logo";
 import { NavItem } from "./NavItem";
 import { UsedSpaceWidget } from "./UsedSpaceWidget";
 import { Profile } from "./Profile";
+import { InputRoot, InputControl, InputPrefix } from "../Input";
 
 export function Sidebar() {
   return (
     <aside className="border-r border-zinc-200 px-5 pt-8 pb-8 flex flex-col gap-6">
       <Logo />
-      <div className="w-full mx-1 flex items-center gap-2 border border-zinc-300 rounded-lg px-2 py-2 shadow-sm">
-        <Search className="w-5 h-5 text-zinc-500" />
-        <input
-          className="flex-1 border-0 bg-transparent p-0 text-zinc-900 placeholder:text-zinc-600 outline-none"
-          placeholder="Search"
-          type="text"
-        />
-      </div>
+      <InputRoot>
+        <InputPrefix>
+          <Search className="h-5 w-5 text-zinc-500" />
+        </InputPrefix>
+        <InputControl placeholder="Search" type="text" />
+      </InputRoot>
       <nav className="space-y-0.5">
         <NavItem title="Home" icon={Home} />
         <NavItem title="Dashboard" icon={BarChart} />
