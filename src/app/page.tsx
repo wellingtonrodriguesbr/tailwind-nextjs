29f2cd777b8bea436f9eaaba4e6eb59859c2e87a
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-10-01 07:42:46 -0300

    feat: photo input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Mail } from "lucide-react";
+import { Mail, UploadCloud, User } from "lucide-react";
 import { SettingsTabs } from "../components/SettingsTabs";
 import { InputControl, InputPrefix, InputRoot } from "../components/Input";
 
@@ -76,7 +76,7 @@ export default function Home() {
 
           <div className="grid grid-cols-form gap-3 pt-5">
             <label
-              htmlFor="email"
+              htmlFor="photo"
               className="text-sm font-medium text-zinc-700"
             >
               Your photo
@@ -84,6 +84,31 @@ export default function Home() {
                 This will be displayed on your profile.
               </span>
             </label>
+            <div className="flex items-start gap-5">
+              <div className="bg-violet-50 flex h-16 w-16 items-center justify-center rounded-full">
+                <User className="h-8 w-8 text-violet-500" />
+              </div>
+              <label
+                htmlFor="photo"
+                className="flex-1 cursor-pointer flex flex-col items-center gap-3 rounded-lg border border-zinc-300 px-6 py-4 text-center text-zinc-500 shadow-sm group"
+              >
+                <div className="rounded-full border-[6px] border-zinc-50 bg-zinc-100 p-2 group-hover:border-violet-50 group-hover:bg-violet-100 transition-colors">
+                  <UploadCloud className="h-5 w-5 text-zinc-600 group-hover:text-violet-600" />
+                </div>
+                <div className="flex flex-col items-center gap-1">
+                  <span className="text-sm">
+                    <span className="text-violet-700 font-semibold">
+                      Click to upload
+                    </span>{" "}
+                    or drag and drop
+                  </span>
+                  <span className="text-xs">
+                    SVG, PNG, JPP or GIF (max. 800x400px)
+                  </span>
+                </div>
+              </label>
+              <input className="sr-only" type="file" id="photo" />
+            </div>
             <div />
           </div>
 
