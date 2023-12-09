39eb4be0f43ce3c31948582182434477e00bd96d
Wellington Rodrigues <wr_rodrigues@outlook.com.br>
2023-12-09 10:49:02 -0300

    feat: add select timezone and country

--- src/components/Form/Input/Root.tsx
+++ src/components/Form/Input/Root.tsx
@@ -5,7 +5,7 @@ interface RootProps extends ComponentProps<"div"> {}
 export function Root({ ...props }: RootProps) {
   return (
     <div
-      className="w-full mx-1 flex items-center gap-2 border border-zinc-300 rounded-lg px-2 py-2 shadow-sm"
+      className="w-full flex items-center gap-2 border border-zinc-300 rounded-lg px-2 py-2 shadow-sm"
       {...props}
     ></div>
   );
--- src/components/Form/Select/index.tsx
+++ src/components/Form/Select/index.tsx
@@ -1,16 +1,19 @@
 "use client";
 
 import * as RadixSelect from "@radix-ui/react-select";
-import { Check, ChevronDown } from "lucide-react";
+import { ChevronDown } from "lucide-react";
+import { ReactNode } from "react";
 
-export function Select() {
+interface SelectProps {
+  children: ReactNode;
+  placeholder: string;
+}
+
+export function Select({ children, placeholder }: SelectProps) {
   return (
     <RadixSelect.Root>
       <RadixSelect.Trigger className="flex h-11 w-full items-center justify-between gap-2 rounded-lg border border-zinc-300 px-3 py-2 shadow-sm data-[placeholder]:text-zinc-500">
-        <RadixSelect.Value
-          placeholder="Select a country..."
-          className="text-black"
-        />
+        <RadixSelect.Value placeholder={placeholder} className="text-black" />
 
         <RadixSelect.Icon>
           <ChevronDown className="h-5 w-5 text-zinc-500" />
@@ -24,19 +27,7 @@ export function Select() {
           position="popper"
           className="z-10 rounded-lg border border-zinc-200 bg-white w-[--radix-select-trigger-width] overflow-hidden"
         >
-          <RadixSelect.Viewport>
-            <RadixSelect.Item
-              value="us"
-              className="flex items-center justify-between gap-2 px-3 py-2.5 outline-none data-[highlighted]:bg-zinc-50"
-            >
-              <RadixSelect.ItemText className="text-black">
-                United States
-              </RadixSelect.ItemText>
-              <RadixSelect.ItemIndicator>
-                <Check className="h-4 w-4 text-violet-500" />
-              </RadixSelect.ItemIndicator>
-            </RadixSelect.Item>
-          </RadixSelect.Viewport>
+          <RadixSelect.Viewport>{children}</RadixSelect.Viewport>
         </RadixSelect.Content>
       </RadixSelect.Portal>
     </RadixSelect.Root>
